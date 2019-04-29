bea78195d70cf4bc53a2cdee71872d0c42b63358
sshleifer <sam@kensho.com>
2019-04-29 11:09:48 -0700

    fix imports

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -2,8 +2,8 @@ from builtins import range
 from builtins import object
 import numpy as np
 
-from layers import *
-from layer_utils import *
+from cs231n.layers import *
+from cs231n.layer_utils import *
 
 
 class TwoLayerNetOrig(object):
