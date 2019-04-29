3ec476a7fe7f32887d4f7635eff82f6c873d9a2b
sshleifer <sam@kensho.com>
2019-04-29 00:35:07 -0700

    copy fc_net

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -6,7 +6,7 @@ from cs231n.layers import *
 from cs231n.layer_utils import *
 
 
-class TwoLayerNet(object):
+class TwoLayerNetOrig(object):
     """
     A two-layer fully-connected neural network with ReLU nonlinearity and
     softmax loss that uses a modular layer design. We assume an input dimension
@@ -116,6 +116,80 @@ class TwoLayerNet(object):
 
         return loss, grads
 
+
+class TwoLayerNet(object):
+    """
+    A two-layer fully-connected neural network with ReLU nonlinearity and
+    softmax loss that uses a modular layer design. We assume an input dimension
+    of D, a hidden dimension of H, and perform classification over C classes.
+
+    The architecure should be affine - relu - affine - softmax.
+
+    Note that this class does not implement gradient descent; instead, it
+    will interact with a separate Solver object that is responsible for running
+    optimization.
+
+    The learnable parameters of the model are stored in the dictionary
+    self.params that maps parameter names to numpy arrays.
+    """
+
+    def __init__(self, input_dim=3 * 32 * 32, hidden_dim=100, num_classes=10,
+                 weight_scale=1e-3, reg=0.0):
+        """
+        Initialize a new network.
+
+        Inputs:
+        - input_dim: An integer giving the size of the input
+        - hidden_dim: An integer giving the size of the hidden layer
+        - num_classes: An integer giving the number of classes to classify
+        - dropout: Scalar between 0 and 1 giving dropout strength.
+        - weight_scale: Scalar giving the standard deviation for random
+          initialization of the weights.
+        - reg: Scalar giving L2 regularization strength.
+        """
+        self.params = {}
+        self.reg = reg
+        self.W1 = np.random.randn(input_dim, hidden_dim) * weight_scale
+        self.b1 = np.zeros(hidden_dim)
+        self.W2 = np.random.randn(hidden_dim, num_classes) * weight_scale
+        self.b2 = np.zeros(num_classes)
+        self.params = {'W1': self.W1, 'W2': self.W2, 'b1': self.b1, 'b2': self.b2}
+
+    def loss(self, X, y=None):
+        """
+        Compute loss and gradient for a minibatch of data.
+
+        Inputs:
+        - X: Array of input data of shape (N, d_1, ..., d_k)
+        - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+        Returns:
+        If y is None, then run a test-time forward pass of the model and return:
+        - scores: Array of shape (N, C) giving classification scores, where
+          scores[i, c] is the classification score for X[i] and class c.
+
+        If y is not None, then run a training-time forward and backward pass and
+        return a tuple of:
+        - loss: Scalar value giving the loss
+        - grads: Dictionary with the same keys as self.params, mapping parameter
+          names to gradients of the loss with respect to those parameters.
+        """
+        x, cache1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+        scores, cache2 = affine_forward(x, self.params['W2'], self.params['b2'])
+
+        if y is None:  # test mode
+            return scores
+        loss, dout = softmax_loss(scores, y)
+        grads = {}
+        reg_losses = [np.sum(v * v) for k, v in self.params.items()
+                      if k in ['W1', 'W2']]
+        reg_loss = np.sum(reg_losses) * self.reg * .5
+        loss += reg_loss
+        dx, dw2, db2 = affine_backward(dout, cache2)
+        grads = dict(W2=dw2 + self.reg * self.params['W2'], b2=db2)
+        _, dw1, db1 = affine_relu_backward(dx, cache1)
+        grads.update(dict(W1=dw1 + self.reg * self.params['W1'], b1=db1))
+        return loss, grads
 
 class FullyConnectedNet(object):
     """
