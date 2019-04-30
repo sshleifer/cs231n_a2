7c8f4a45081c8e2046b0cf297f021089c4986d5d
sshleifer <sam@kensho.com>
2019-04-30 10:40:03 -0700

    add failing tests

--- assignment2/cs231n/test_layers.py
+++ assignment2/cs231n/test_layers.py
@@ -363,3 +363,64 @@ class TestConv(unittest.TestCase):
                         verbose=True, print_every=1)
         solver.train()
         self.assertGreaterEqual(solver.train_acc_history[-1], .6)
+
+
+    def test_spatial_batchnorm_fwd(self):
+        np.random.seed(231)
+        # Check the training-time forward pass by checking means and variances
+        # of features both before and after spatial batch normalization
+
+        N, C, H, W = 2, 3, 4, 5
+        x = 4 * np.random.randn(N, C, H, W) + 10
+
+        print('Before spatial batch normalization:')
+        print('  Shape: ', x.shape)
+        print('  Means: ', x.mean(axis=(0, 2, 3)))
+        print('  Stds: ', x.std(axis=(0, 2, 3)))
+
+        # Means should be close to zero and stds close to one
+        gamma, beta = np.ones(C), np.zeros(C)
+        bn_param = {'mode': 'train'}
+        out, _ = spatial_batchnorm_forward(x, gamma, beta, bn_param)
+        print('After spatial batch normalization:')
+        print('  Shape: ', out.shape)
+        print('  Means: ', out.mean(axis=(0, 2, 3)))
+        print('  Stds: ', out.std(axis=(0, 2, 3)))
+
+        # Means should be close to beta and stds close to gamma
+        gamma, beta = np.asarray([3, 4, 5]), np.asarray([6, 7, 8])
+        out, _ = spatial_batchnorm_forward(x, gamma, beta, bn_param)
+        print('After spatial batch normalization (nontrivial gamma, beta):')
+        print('  Shape: ', out.shape)
+        mns = out.mean(axis=(0, 2, 3))
+        self.assertTrue((np.round(mns, 1) == gamma).all())
+        self.assertTrue((np.round(out.std(axis=(0, 2, 3)), 1)==gamma).all())
+        print('  Means: ', out.mean(axis=(0, 2, 3)))
+        print('  Stds: ', out.std(axis=(0, 2, 3)))
+
+
+    def test_spatial_batchnorm_bwd(self):
+        np.random.seed(231)
+        N, C, H, W = 2, 3, 4, 5
+        x = 5 * np.random.randn(N, C, H, W) + 12
+        gamma = np.random.randn(C)
+        beta = np.random.randn(C)
+        dout = np.random.randn(N, C, H, W)
+
+        bn_param = {'mode': 'train'}
+        fx = lambda x: spatial_batchnorm_forward(x, gamma, beta, bn_param)[0]
+        fg = lambda a: spatial_batchnorm_forward(x, gamma, beta, bn_param)[0]
+        fb = lambda b: spatial_batchnorm_forward(x, gamma, beta, bn_param)[0]
+
+        dx_num = eval_numerical_gradient_array(fx, x, dout)
+        da_num = eval_numerical_gradient_array(fg, gamma, dout)
+        db_num = eval_numerical_gradient_array(fb, beta, dout)
+
+        # You should expect errors of magnitudes between 1e-12~1e-06
+        _, cache = spatial_batchnorm_forward(x, gamma, beta, bn_param)
+        dx, dgamma, dbeta = spatial_batchnorm_backward(dout, cache)
+        ACCEPTABLE = 1e-6
+        self.assertGreaterEqual(ACCEPTABLE, rel_error(dx_num, dx))
+        self.assertGreaterEqual(ACCEPTABLE, rel_error(da_num, dgamma))
+        self.assertGreaterEqual(ACCEPTABLE, rel_error(db_num, dbeta))
+
